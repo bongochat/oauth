f21bad916f2b8b90d5d6862be6bfd1e27d5eb9b3
sajib1066 <sajib1066@gmail.com>
2024-05-20 00:32:25 +0800

    update client id data type

--- domain/devices/devices_dto.go
+++ domain/devices/devices_dto.go
@@ -8,7 +8,7 @@ import (
 type Devices struct {
 	AccessToken string    `json:"access_token"`
 	UserId      int64     `json:"user_id"`
-	ClientId    int64     `json:"client_id,omitempty"`
+	ClientId    string    `json:"client_id,omitempty"`
 	DeviceId    string    `json:"device_id"`
 	DeviceType  string    `json:"device_type"`
 	DeviceModel string    `json:"device_model"`
