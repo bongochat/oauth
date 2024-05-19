be73e5df1f81abda1badfdabdb28169a08c4de46
sajib1066 <sajib1066@gmail.com>
2024-05-20 00:30:14 +0800

    update device verification

--- domain/verify_device/device_dto.go
+++ domain/verify_device/device_dto.go
@@ -5,7 +5,7 @@ import "time"
 type VerifyDevice struct {
 	AccessToken string    `json:"access_token"`
 	UserId      int64     `json:"user_id"`
-	ClientId    int64     `json:"client_id,omitempty"`
+	ClientId    string    `json:"client_id,omitempty"`
 	DeviceId    string    `json:"device_id"`
 	IsVerified  bool      `json:"is_verified"`
 	DateCreated time.Time `json:"date_created"`
