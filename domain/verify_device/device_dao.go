a2d10f315584836acccbcd51787706e07f16686a
sajib1066 <sajib1066@gmail.com>
2025-10-09 03:53:59 +0800

    update verify device api

--- domain/verify_device/device_dao.go
+++ domain/verify_device/device_dao.go
@@ -2,12 +2,14 @@ package verify_device
 
 import (
 	"context"
+	"errors"
 
 	"github.com/bongochat/oauth/clients/mongodb"
 	"github.com/bongochat/oauth/domain/access_token"
 	"github.com/bongochat/oauth/logger"
 	"github.com/bongochat/utils/resterrors"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -19,13 +21,16 @@ func (r VerifyDevice) VerifyDevice(accountNumber int64, token string) (*VerifyDe
 		return nil, resterrors.NewInternalServerError("Invalid access token", "", err)
 	}
 
-	filter := bson.M{"accesstoken": token}
+	filter := bson.M{"accesstoken": token, "accountnumber": accountNumber}
 	update := bson.M{
 		"$set": bson.M{"isverified": true},
 	}
 	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
 
 	if err := mongodb.GetCollections().FindOneAndUpdate(context.Background(), filter, update, opts).Decode(&result); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, resterrors.NewUnauthorizedError("Access token is not valid", "Access token is not valid")
+		}
 		logger.ErrorLog(err)
 		return nil, resterrors.NewInternalServerError("Database error", "", err)
 	}
